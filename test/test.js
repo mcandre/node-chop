c27f0a9bc4e9f7faaf7145a427ebc72312f58dc3
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2018-11-11 18:36:22 -0600

    update editorconfig

--- test/test.js
+++ test/test.js
@@ -5,34 +5,34 @@ assert = require("assert"),
 chop = require("../lib/chop");
 
 describe("chop", function() {
-  describe("chop", function() {
-    it("should handle EOLs", function() {
-      assert.equal("abc", chop.chop("abc\n"));
-      assert.equal("abc", chop.chop("abc\r"));
-      assert.equal("abc\r", chop.chop("abc\r\n"));
+    describe("chop", function() {
+        it("should handle EOLs", function() {
+            assert.equal("abc", chop.chop("abc\n"));
+            assert.equal("abc", chop.chop("abc\r"));
+            assert.equal("abc\r", chop.chop("abc\r\n"));
+        });
+
+        it("should handle no EOLs", function() {
+            assert.equal("ab", chop.chop("abc"));
+            assert.equal("a", chop.chop("ab"));
+            assert.equal("", chop.chop("a"));
+            assert.equal("", chop.chop(""));
+        });
     });
 
-    it("should handle no EOLs", function() {
-      assert.equal("ab", chop.chop("abc"));
-      assert.equal("a", chop.chop("ab"));
-      assert.equal("", chop.chop("a"));
-      assert.equal("", chop.chop(""));
-    });
-  });
+    describe("chomp", function() {
+        it("should handle EOLs", function() {
+            assert.equal("abc", chop.chomp("abc\n"));
+            assert.equal("abc", chop.chomp("abc"));
+            assert.equal("abc", chop.chomp("abc\r"));
+            assert.equal("abc", chop.chomp("abc\r\n"));
+        });
 
-  describe("chomp", function() {
-    it("should handle EOLs", function() {
-      assert.equal("abc", chop.chomp("abc\n"));
-      assert.equal("abc", chop.chomp("abc"));
-      assert.equal("abc", chop.chomp("abc\r"));
-      assert.equal("abc", chop.chomp("abc\r\n"));
+        it("should handle no EOLs", function() {
+            assert.equal("abc", chop.chomp("abc"));
+            assert.equal("ab", chop.chomp("ab"));
+            assert.equal("a", chop.chomp("a"));
+            assert.equal("", chop.chomp(""));
+        });
     });
-
-    it("should handle no EOLs", function() {
-      assert.equal("abc", chop.chomp("abc"));
-      assert.equal("ab", chop.chomp("ab"));
-      assert.equal("a", chop.chomp("a"));
-      assert.equal("", chop.chomp(""));
-    });
-  });
 });
